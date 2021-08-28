ccbfb6168b76b2f660c5ec9c33b805078816aac3
Vadim Kireev <tudomundoze@yandex.ru>
2021-08-28 17:07:28 +0300

    first review commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,39 @@
+// Объявляем необходимые переменные
 let profileEditButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let popupContainer = document.querySelector('.popup__container');
-let popupSubmitButton = document.querySelector('.popup__submit-button');
-let inputName = popupContainer.querySelectorAll('.popup__input')[0];
-let inputJob = popupContainer.querySelectorAll('.popup__input')[1];
+let popupProfileInfo = document.querySelector('#popup-profile-info');
+let inputName = popupProfileInfo.querySelector('#popup-name');
+let inputJob = popupProfileInfo.querySelector('#popup-job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
-function togglePopup() {
+/* При открытии попапа, значения полей ввода становятся
+равными значениям имени и профессии со страницы */
+function openPopup() {
+  popup.classList.toggle('popup_opened');
+  inputName.value = profileTitle.textContent;
+  inputJob.value = profileSubtitle.textContent;
+}
+
+// функция закрывающая попап
+function closePopup() {
   popup.classList.toggle('popup_opened');
 }
 
-profileEditButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
+// слушатели кликов кнопок редактирования профиля и закрытия, вызывающие соответствующие функции
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 
+/* Сначала функция получает событие, которое происходит по умолчанию по нажатию кнопки submit,
+затем она предотвращает это событие по умолчанию (отправка данных на сервер). Далее присваивает значения
+введенные в поля input'ы соответствующим элементам на странице и закрывает попап */
 function formSubmitHandler(event) {
   event.preventDefault();
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputJob.value;
+  closePopup();
 }
 
-popupContainer.addEventListener('submit', formSubmitHandler);
-
-popupSubmitButton.addEventListener('click', togglePopup);
+// Добавляем к попап-форме слушателя события submit и запускаем функцию
+popupProfileInfo.addEventListener('submit', formSubmitHandler);
